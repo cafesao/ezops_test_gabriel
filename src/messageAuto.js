b98363f74cb3facdde5ea259f5782df58cf76c57
Gabriel <gabriel.diasdutra2@gmail.com>
2020-11-14 19:11:28 -0300

    feat: Adicionando mensagens automaticas

---
+++ src/messageAuto.js
@@ -0,0 +1,36 @@
+const name = 'Sistema'
+
+module.exports = {
+  messageOla: {
+    name,
+    message: 'Ola a todos!!',
+  },
+  messageAdeus: {
+    name,
+    message: 'Adeus, espero que volte!!',
+  },
+  messageStatus: {
+    name,
+    message: 'Sistema operante e a todo vapor!',
+  },
+  messageGosto: {
+    name,
+    message: 'Eu gosto de conversar com vocês',
+  },
+  messageSistema: {
+    name,
+    message: 'Utilizo o sistema Ubuntu, em uma instância na AWS',
+  },
+  messageMestre: {
+    name,
+    message: 'Quem me criou foi meu mestre Cafesao :)',
+  },
+  messageGit: {
+    name,
+    message: 'GITHUB: https://github.com/cafesao',
+  },
+  messageRepo: {
+    name,
+    message: 'Repositório: https://github.com/cafesao/ezops_test_gabriel',
+  },
+}
